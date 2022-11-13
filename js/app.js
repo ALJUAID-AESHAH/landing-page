b96adc7e679aef945a80fa9253994c8c3105386c
ALJUAID-AESHAH <AljueidAishah@outlook.sa>
2022-11-13 23:06:50 +0300

    update

--- js/app.js
+++ js/app.js
@@ -41,7 +41,7 @@ for (const section of sections) {
     const item = document.createElement('li');
     const name = section.getAttribute('data-nav');
     const id = section.getAttribute('id')
-    item.innerHTML = `<a class="menu__link" href="#${id}">${name}</a>`
+    item.innerHTML = `<a class="menu__link ${id}" >${name}</a>`
     navItems.appendChild(item)
 }
 
@@ -51,28 +51,32 @@ for (const section of sections) {
 // Begin Events
 
 // Scroll to anchor ID using scrollTO event
-
 const anchors = document.querySelectorAll('.menu__link')
 anchors.forEach((value, key) => {
     value.addEventListener('click', function (event) {
         event.preventDefault();
-        // Scroll to section on link click
         const topPos = sections[key].getBoundingClientRect().top + window.pageYOffset
         window.scrollTo({
             top: topPos,
             behavior: 'smooth'
         });
     })
-
 })
 
+
+
 // Add class 'active' to section when near top of viewport
+// highlight navbar item
 window.addEventListener('scroll', () => {
     for (const section of sections) {
-        if (section.getBoundingClientRect().top >= -597 && section.getBoundingClientRect().top <= 270) {
+        const id = section.getAttribute('id')
+        let temp = document.getElementsByClassName(id)
+        if (section.getBoundingClientRect().top >= -500 && section.getBoundingClientRect().top <= 270) {
             section.classList.add("active")
+            temp[0].classList.add("actived")
         } else {
             section.classList.remove("active")
+            temp[0].classList.remove("actived")
         }
     }
 })
